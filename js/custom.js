8cc405265ca017b0beb47eaafec0610f888cd554
z3nth10n <z3nth10n@gmail.com>
2021-01-31 16:26:43 +0100

    portfolio finished

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
  // MDB Lightbox Init
+/*
         $(function () {
             $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
         });
@@ -8,3 +9,29 @@
 
         // Animations Init
         new WOW().init();
+*/
+
+var slides;
+var slide_count = 0;
+var slide = 0;
+$(document).ready(function() {
+    slides = $(".carousel-item");
+    slide_count = slides.length;
+    console.log(slides);
+    
+    setInterval(function() {
+        // var s = slide++;
+        slides[slide].setAttribute('class', 'carousel-item');
+        // slides[slide].toggle("slide:left");
+        
+        /* console.log(slide);
+        ++slide;
+        console.log(slide); */
+        
+        ++slide;
+        if(slide >= slide_count) slide = 0;
+        
+        slides[slide].setAttribute('class', 'carousel-item active');
+        // slides[slide].toggle("slide:right");
+    }, 5000);
+});
